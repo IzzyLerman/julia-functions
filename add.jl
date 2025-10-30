5dd294a7adba4ced12241e5f8c98b3808037eba7
Izzy Lerman <izzylerman14@gmail.com>
2025-10-30 14:44:16 -0700

    update add with rpc

--- add.jl
+++ add.jl
@@ -1,5 +1,11 @@
-function add(args)
-    num1 = args["num1"]
-    num2 = args["num2"]
+function add(num1, num2)
     num3 = num1 + num2
-    print("add: $num1 + $num2 = $num3")
+    println("add: $num1 + $num2 = $num3")
+
+    Base.write("/tmp/output.txt", string(num3))
+    try
+        faasr_put_file("/tmp/output.txt", "add_output.txt")
+    catch e 
+        println("Exception with faasr_put_file: $(typeof(e)) $(e) ")
+    end
+end
